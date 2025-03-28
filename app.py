edcdbc9ab43a578353f5e8bb55e46da3f7687697
konkal-shirisha <chinnukonkal0122@gmail.com>
2025-03-28 18:06:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier()
 app = Flask(__name__)
-model = joblib.load('Product_Ratings_Dataset.xls.pkl')
+model = joblib.load('Product_Ratings_Dataset.xls')
+model = joblib.load('Product_Recommendation_Dataset.xls.pkl')
 @app.route('/')
 def home():
     return render_template('home.html')
